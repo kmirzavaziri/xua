935bfb715b5a823b702632d917058b2982041349
kmirzavaziri <k.mirzavaziri@digikala.com>
2020-12-17 23:43:10 +0330

    Started xua doc project

---
+++ xuadoc/ReMatcher.py
@@ -0,0 +1,12 @@
+import re
+
+class ReMatcher(object):
+    def __init__(self, matchstring):
+        self.matchstring = matchstring
+
+    def match(self,regexp):
+        self.rematch = re.match(regexp, self.matchstring)
+        return bool(self.rematch)
+
+    def group(self,i):
+        return self.rematch.group(i)
